70827aa3b12c28bc9c3e29da3175b8f5496b3218
savolkov <volkovsema@gmail.com>
2017-04-20 23:57:29 +0500

    Remake

--- js/my.js
+++ js/my.js
@@ -1,7 +1,83 @@
 
 var script = document.createElement('script');
 script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js';
-function tableRecord(number, dateTime, fullName, status, tab) { //получаю <tr>
+
+ComplaintStatus = { //Статус (Поступила; В обработке; Перенаправлена; Обработана; Закрыта)
+    recieved : "Поступила",
+    inWork : "В обработке",
+    reAddressed : "Перенаправлена",
+    done : "Обработана",
+    closed : "Закрыта"
+};
+
+records = [
+    {
+        ved: "Ведомство 1",
+        ID: 12,
+        dateTime: "19.04.2017",
+        fullName: "Иванов Иван Иваныч",
+        status: ComplaintStatus.recieved
+    },
+    {
+        ved: "Ведомство 2",
+        ID: 2,
+        dateTime: "19.04.2017",
+        fullName: "Иванов Иван Иваныч",
+        status: ComplaintStatus.recieved
+    },
+    {   
+        ved: "Ведомство 3",
+        ID: 3,
+        dateTime: "19.04.2017",
+        fullName: "Иванов Иван Иваныч",
+        status: ComplaintStatus.recieved
+    },
+    {   
+        ved: "Ведомство 5",
+        ID: 5,
+        dateTime: "19.04.2017",
+        fullName: "Иванов Иван Иваныч",
+        status: ComplaintStatus.recieved
+    },
+    {   
+        ved: "Ведомство 1",
+        ID: 1123,
+        dateTime: "19.04.2017",
+        fullName: "Иванов Иван Иваныч-dnjhj",
+        status: ComplaintStatus.recieved
+    }
+];
+Tabs = []
+
+function ifInArray(item, array){
+    for(a in array){
+        if(item===array[a]){
+            return true;
+        }
+    }
+    return false;
+}
+function buildMenu(){
+    for(item in records){
+        currentItem = records[item];
+        if(!(ifInArray(currentItem.ved, Tabs))){
+            Tabs.push(currentItem.ved);
+        }
+    }
+    flag = false;
+    for(tab in Tabs){
+        currentTab = Tabs[tab];
+        var html = $("#mainMenu").html()
+        html+='<li><a href="#">'+currentTab+' <span class="label label-success">Обработана</span></a></li>';
+        $("#mainMenu").html(html)
+        if(flag === false){
+            flag = true;
+            $("#mainMenu li").addClass("active");
+        }
+    }
+}
+
+function tableRecord(number, dateTime, fullName, status) { //получаю <tr>
     var statusString;
     if(status===ComplaintStatus.recieved){statusString = '<span class="label label-warning">'+status+'</span>';}
     if(status===ComplaintStatus.inWork){statusString = '<span class="label label-info">'+status+'</span>';}
@@ -14,91 +90,17 @@ function tableRecord(number, dateTime, fullName, status, tab) { //получаю
     html+='<td><div style=\"float: left\"><div class=\"dropdown\" style=\"display: inline-block\"><a data-toggle=\"dropdown\" href=\"#\">Переместить</a><ul class=\"dropdown-menu\" role=\"menu\" aria-labelledby=\"dLabel\"><li>Ved1</li><li>Ved2</li></ul></div><a href=\"#\"><i class=\"glyphicon glyphicon-trash\"></i></a></div></td></tr>';
     return html;
 }
-ComplaintStatus = { //Статус (Поступила; В обработке; Перенаправлена; Обработана; Закрыта)
-    recieved : "Поступила",
-    inWork : "В обработке",
-    reAddressed : "Перенаправлена",
-    done : "Обработана",
-    closed : "Закрыта"
-}
 
-Vedomstva = { //все ведомства
-ved1: "Ведомство 1",
-ved2: "Ведомство 2",
-ved3: "Ведомство 3"
-}
-// var tableItem = {
-//     number,
-//     dateTime,
-//     fullName,
-//     status,
-//     ved
-// }
-tableItems = [
-    {
-        number: 1,
-        dateTime: "19.04.2017",
-        fullName: "Иванов Иван Иваныч",
-        status: ComplaintStatus.recieved,
-        ved: Vedomstva.ved1
-    },
-    {
-        number: 2,
-        dateTime: "19.04.2017",
-        fullName: "Иванов Иван Иваныч",
-        status: ComplaintStatus.done,
-        ved: Vedomstva.ved1
-    },
-    {
-        number: 3,
-        dateTime: "19.04.2017",
-        fullName: "Иванов Иван Иваныч",
-        status: ComplaintStatus.inWork,
-        ved: Vedomstva.ved2
-    },
-    {
-        number: 4,
-        dateTime: "19.04.2017",
-        fullName: "Иванов Иван Иваныч",
-        status: ComplaintStatus.recieved,
-        ved: Vedomstva.ved3
-    }
-    
-]
-function getTab(){ //построение вкладки с таблицей
-    var resString = "";
-    for(item in Vedomstva)
-    {   
-        resString += '<div class="tab-pane fade in" id="'+item+'">';
-        resString+='<h2>'+Vedomstva[item]+'</h2><hr><table class="table table-striped table-hover"><col width="10%"><col width="10%"><col width="30%">';
-        resString+='<col width="20%"><col width="30%"><thead><tr><th>#</th><th>Дата</th><th>ФИО заявителя</th><th>Статус</th><th>Действия</th></tr>';
-        resString+='</thead><tbody></tbody></table></div>';}
-        return resString;
-    }
-function fillInTable(){
-    var ved1Data = '';
-    var ved2Data = '';
-    var ved3Data = '';
-    for(item in tableItems){
-
-        var wat = tableItems[item]; 
-
-        var html = tableRecord(wat.number, wat.dateTime, wat.fullName, wat.status, wat.ved)
-        if(wat.ved === Vedomstva.ved1){
-            ved1Data=$("#ved1 tbody").html();
-            ved1Data+=html;
-            $("#ved1 tbody").html(ved1Data);
+function buildTable(name){
+    $("#tableContext").html("");
+    $("#tableHeading").html(name);
+    var html = "";
+    for(item in records){
+        currentItem = records[item];
+        if(currentItem.ved===name){
+            html += $("#tableContext").html()
+            html+=tableRecord(currentItem.ID, currentItem.dateTime, currentItem.fullName, currentItem.status)
         }
-        if(wat.ved === Vedomstva.ved2){
-            ved2Data=$("#ved2 tbody").html();
-            ved2Data+=html;
-            $("#ved2 tbody").html(ved2Data);
-        }
-        if(wat.ved === Vedomstva.ved3){
-            ved3Data=$("#ved3 tbody").html();
-            ved3Data+=html;
-            $("#ved3 tbody").html(ved3Data);
-        }
-        
     }
+    $("#tableContext").html(html);
 }
